88994c9d054c6ff7aebb29320dd90e02bf66bda9
WONGJE4 <wongje4@oocl.com>
2022-12-15 15:27:58 +0800

    test: finish should_return_updated_todo_done_when_perform_put_given_todo_id_and_done

--- src/test/java/com/afs/todolist/springbootTodo/TodoControllerTest.java
+++ src/test/java/com/afs/todolist/springbootTodo/TodoControllerTest.java
@@ -2,15 +2,20 @@ package com.afs.todolist.springbootTodo;
 
 import com.afs.todolist.entity.Todo;
 import com.afs.todolist.repository.TodoRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,6 +44,31 @@ public class TodoControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").isString())
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].text").value("testing text1"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$[0].done").value(true));
+    }
+
+    @Test
+    void should_return_updated_todo_done_when_perform_put_given_todo_id_and_done() throws Exception {
+        //given
+        String todoId = new ObjectId().toString();
+        todoRepository.save(new Todo(todoId, "testing text1", true));
+        Todo updateTodo = (new Todo(todoId, null, false));
+
+        String updateTodoJson = new ObjectMapper().writeValueAsString(updateTodo);
+
+        //when
+        client.perform(MockMvcRequestBuilders.put("/todos/{id}", todoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(updateTodoJson))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").isString())
+                .andExpect(MockMvcResultMatchers.jsonPath("text").value("testing text1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("done").value(false));
+
+        // then
+        final Todo updatedTodo = todoRepository.findAll().get(0);
+        assertThat(updatedTodo.getText(), equalTo("testing text1"));
+        assertThat(updatedTodo.getDone(), equalTo(false));
+
     }
 
 
